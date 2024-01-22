47edc331be032290c8e08874a8f0e33b4abb6799
Rush Perera <104535750+Rush-Perera@users.noreply.github.com>
2024-01-22 17:33:40 +0530

    gtag

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -1,10 +1,23 @@
+import Script from "next/script";
+
 export default function Head() {
   return (
     <>
       <title>GENZ Technologies</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Built with Next.js and TypeScript" />
+      <meta name="description" content="This is the official Web Site of GENZ Technologies Pvt) Ltd. Built with Next.js and TypeScript" />
       <link rel="icon" href="/images/favicon.ico" />
+
+     
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-DCZ074FP07"></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag("config", "G-DCZ074FP07");
+        `}
+      </Script>
     </>
   );
 }
